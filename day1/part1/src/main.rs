46c3b116723e12ac27a8e77ae7d995e85ff82096
Radoslav Georgiev <rgeorgiev583@gmail.com>
2021-12-12 19:55:50 +0200

    refactor: rename some identifiers

--- day1/part1/src/main.rs
+++ day1/part1/src/main.rs
@@ -6,16 +6,16 @@ use std::{
 fn main() -> Result<(), Box<dyn Error>> {
     let stdin = stdin();
     let stdin = BufReader::new(stdin);
-    let mut previous: Option<usize> = None;
+    let mut previous_measurement: Option<usize> = None;
     let mut num_increments: usize = 0;
     for line in stdin.lines() {
-        let current = line?.parse::<usize>()?;
-        if let Some(previous) = previous {
-            if current > previous {
+        let current_measurement = line?.parse::<usize>()?;
+        if let Some(previous_measurement) = previous_measurement {
+            if current_measurement > previous_measurement {
                 num_increments += 1;
             }
         }
-        previous = Some(current);
+        previous_measurement = Some(current_measurement);
     }
     println!("{}", num_increments);
     Ok(())
