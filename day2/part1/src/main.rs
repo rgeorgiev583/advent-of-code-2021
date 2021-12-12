8540e014fa25b420d32a58292024ef3cef0cdd16
Radoslav Georgiev <rgeorgiev583@gmail.com>
2021-12-12 21:20:38 +0200

    add solution to part one of the second day task

---
+++ day2/part1/src/main.rs
@@ -0,0 +1,43 @@
+use core::fmt;
+use std::{
+    error::Error as ErrorTrait,
+    io::{stdin, BufRead, BufReader},
+};
+
+use scanf::sscanf;
+
+#[derive(Debug)]
+enum Error {
+    InvalidCommand(String),
+}
+
+impl fmt::Display for Error {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Self::InvalidCommand(command) => write!(f, "invalid command: {}", command),
+        }
+    }
+}
+
+impl ErrorTrait for Error {}
+
+fn main() -> Result<(), Box<dyn ErrorTrait>> {
+    let stdin = stdin();
+    let stdin = BufReader::new(stdin);
+    let mut depth: isize = 0;
+    let mut horizontal_position: isize = 0;
+    for line in stdin.lines() {
+        let line = line?;
+        let mut command = String::new();
+        let mut difference: isize = 0;
+        sscanf!(line.as_str(), "{} {}", command, difference)?;
+        match command.as_str() {
+            "forward" => horizontal_position += difference,
+            "down" => depth += difference,
+            "up" => depth -= difference,
+            _ => return Err(Box::new(Error::InvalidCommand(command))),
+        };
+    }
+    println!("{}", horizontal_position * depth);
+    Ok(())
+}
